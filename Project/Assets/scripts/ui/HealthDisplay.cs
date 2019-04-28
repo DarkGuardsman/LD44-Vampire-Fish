1a105db3377a5f7059759168e30ada97e9c0912f
Robert WC Seifert <rseifert.phone@gmail.com>
2019-04-28 16:32:02 -0400

    Added health bar

--- Project/Assets/scripts/ui/HealthDisplay.cs
+++ Project/Assets/scripts/ui/HealthDisplay.cs
@@ -6,22 +6,65 @@ using UnityEngine.UI;
 
 public class HealthDisplay : UIDisplay 
 {
-    public Text text;
+     public DamageData damageData;
     
-	private DamageData damageData;
+    public Text healthText;
     
-	void FixedUpdate () 
+    public Image healthImage;
+    
+    private float healthWidth;
+    
+    void Start()
     {
-        if(gameController.currentPlayerObject != null)
+        healthWidth = healthImage.rectTransform.sizeDelta.x;
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if(gameController.currentPlayerObject != null && damageData == null)
         {
-            if(damageData == null)
+            damageData = gameController.currentPlayerObject.GetComponent<DamageData>();
+        }
+        if(damageData != null)
+        {
+            if(healthText != null)
             {
-                damageData = gameController.currentPlayerObject.GetComponent<DamageData>();
+                healthText.text = damageData.health + "/" + damageData.maxHealth;
+            }
+            
+            if(healthImage != null)
+            {            
+                float hpSize = getSize(healthWidth, damageData.health, damageData.maxHealth);
+                healthImage.rectTransform.sizeDelta = new Vector2(hpSize, healthImage.rectTransform.sizeDelta.y);
+            }
+            handleBar(healthImage, healthWidth, damageData.health, damageData.maxHealth);
+        }
+        else if(healthText != null)
+        {
+            healthText.text = "Err";
+        }
+	}
+    
+    private void handleBar(Image image, float normal, float hp, float maxHp)
+    {
+        if(image != null)
+        { 
+            float size = getSize(normal, hp, maxHp);
+            if(size > 0)
+            {
+                image.gameObject.SetActive(true);
+                image.rectTransform.sizeDelta = new Vector2(size, image.rectTransform.sizeDelta.y);
             }
             else
             {
-                text.text =  text.text = String.Format("Health: {0:0}", damageData.health);
+                image.gameObject.SetActive(false);
             }
-        }
-	}
+        }           
+    }
+    
+    private float getSize(float normal, float hp, float maxHp)
+    {        
+        return Mathf.Min(normal, Mathf.Max(0, normal * (hp / maxHp)));
+    }
 }
