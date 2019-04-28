a9cc13e5edd43d8a5acc33203c8939103fa24b6b
Robert WC Seifert <rseifert.phone@gmail.com>
2019-04-28 14:54:53 -0400

    Added darkness mask, Expanded level

---
+++ Project/Assets/scripts/parts/MatchPosition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchPosition : MonoBehaviour {
+
+    public Transform target;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 position = transform.position;
+		position.x = target.position.x;
+        position.y = target.position.y;
+        
+        transform.position = position;
+	}
+}
