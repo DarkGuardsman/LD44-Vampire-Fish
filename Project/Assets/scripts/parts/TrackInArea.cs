d7f2e5b2d06c738e06f9573153298b0f64e10478
Robert WC Seifert <rseifert.phone@gmail.com>
2019-04-28 12:58:35 -0400

    Added attack logic

---
+++ Project/Assets/scripts/parts/TrackInArea.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackInArea : MonoBehaviour {
+
+	public int layerToCheck;
+    public List<GameObject> objectsInArea = new List<GameObject>();
+
+	void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.gameObject.layer == layerToCheck)
+        {
+            objectsInArea.Add(other.gameObject);
+        }
+    }
+    
+    void OnTriggerExit2D(Collider2D other) 
+    {
+        if(other.gameObject.layer == layerToCheck)
+        {
+            objectsInArea.Remove(other.gameObject);
+        }
+    }
+}
---
+++ Project/Assets/scripts/player/PlayerAttack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+    
+    public TrackInArea areaOfAttack;
+    public float damage = 10f;
+	
+    void FixedUpdate()
+    {
+		if (Input.GetMouseButtonDown(0))
+        {
+            //Copy list to prevent concurrent modifications
+            List<GameObject> attackTargets = new List<GameObject>();
+            attackTargets.AddRange(areaOfAttack.objectsInArea);
+            
+            //Loop targets
+            foreach(GameObject target in attackTargets)
+            {
+                //Damage targets
+                DamageData damageData = target.GetComponent<DamageData>();
+                if(damageData != null)
+                {
+                    damageData.Attack(damage, gameObject);
+                }
+            }
+        }
+	}
+}
