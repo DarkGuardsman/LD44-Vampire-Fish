cb6121b25e923547091eedb5b57497cda6899e5b
Robert WC Seifert <rseifert.phone@gmail.com>
2019-04-29 19:56:13 -0400

    Randomized block placement

---
+++ Project/Assets/scripts/map/BlockRandom.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockRandom : MonoBehaviour {
+    
+    public SpriteRenderer[] blocks;
+    
+    public Sprite[] textures;
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < blocks.Length; i++)
+        {
+            blocks[i].sprite = textures[Random.Range(0, textures.Length)];
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
